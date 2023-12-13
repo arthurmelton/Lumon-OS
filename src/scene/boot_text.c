e7580aa032220e65a222b7d34e10a7c1a968c33d
Arthur Melton <git-zl9limki@dontrackme.com>
2023-12-13 11:35:21 -0500

    format

--- src/scene/boot_text.c
+++ src/scene/boot_text.c
@@ -5,22 +5,22 @@
 
 static char startup_print[] =
 	"LUMON OS\n"
-    "ParseElements: keyboard: 0 digitizer: 12 pointer: 0 0 scroll: 0 led: 0\n"
-    "GPU Initialized: Control ID 16\n"
-    "startupTask\n"
-    "Video has output streams: 1\n"
-    "Reuse output buffer index:2 dev:Code Output offset: 1840000 size:20800\n"
-    "Audio has output streams: 1\n"
-    "DisableInput = 0\n"
-    "portType = 3\n"
-    "skipCached = 0\n"
-    "PortType:3, Length: 128";
+	"ParseElements: keyboard: 0 digitizer: 12 pointer: 0 0 scroll: 0 led: 0\n"
+	"GPU Initialized: Control ID 16\n"
+	"startupTask\n"
+	"Video has output streams: 1\n"
+	"Reuse output buffer index:2 dev:Code Output offset: 1840000 size:20800\n"
+	"Audio has output streams: 1\n"
+	"DisableInput = 0\n"
+	"portType = 3\n"
+	"skipCached = 0\n"
+	"PortType:3, Length: 128";
 
 int offset_x = 32;
 int offset_y = 32;
 
 void boot_text(struct flame screen, unsigned long long int frame,
-		  enum state *screen_state) {
+			   enum state *screen_state) {
 	if (*screen_state == BootText) {
 		if (frame == sizeof(startup_print) / sizeof(*startup_print)) {
 			*screen_state = BootAnimation;
--- src/scene/boot_text.h
+++ src/scene/boot_text.h
@@ -2,6 +2,6 @@
 #define BOOT_TEXT
 
 void boot_text(struct flame screen, unsigned long long int frame,
-		  enum state *screen_state);
+			   enum state *screen_state);
 
 #endif
