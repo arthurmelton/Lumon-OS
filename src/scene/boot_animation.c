75e3a340bcd95add7543176f6c0c65df399e65ec
Arthur Melton <git-zl9limki@dontrackme.com>
2023-12-14 14:24:41 -0500

    Set boot text background to transparent

--- src/scene/boot_animation.c
+++ src/scene/boot_animation.c
@@ -16,7 +16,7 @@ void boot_animation(struct flame screen, unsigned long long int frame,
 
 		flame_xbm(screen, (screen.width - LUMON_LOGO_WIDTH) / 2,
 				  (screen.height - LUMON_LOGO_HEIGHT) / 2, LUMON_LOGO,
-				  LUMON_LOGO_WIDTH, LUMON_LOGO_HEIGHT, 0x00FFFFFF, 0x00000000);
+				  LUMON_LOGO_WIDTH, LUMON_LOGO_HEIGHT, 0x00FFFFFF, 0x01000000);
 
 		flame_refresh();
 
